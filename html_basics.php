a2c446a2dc4087250804386e0f1b865be11034fd
suja chackochan <sujachakochan@gmail.com>
2019-10-14 12:43:13 -0400

    add html basics

---
+++ html_basics.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HTML Basics</title>
+</head>
+<body>
+<h1>Heading H1</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ornare pulvinar justo nec gravida. Nulla facilisi. Donec et est porta lorem vestibulum aliquet. Quisque a nunc imperdiet, ultrices arcu a, ultrices nunc. Ut tellus felis, condimentum quis blandit ac, volutpat eget justo. Nunc tortor lacus, suscipit ut nulla nec, mattis interdum nunc. Aenean et rhoncus quam. Etiam vel pellentesque nulla, non dignissim tellus. Cras a tortor et nisi ornare molestie quis et risus. Cras suscipit pharetra magna, sit amet rhoncus quam placerat et. Mauris gravida, nisi ut fermentum maximus, magna libero tempor quam, at porta ante libero eu eros. Praesent nec massa in dui lacinia tempus. In vehicula pulvinar neque, at scelerisque lectus ullamcorper scelerisque. Suspendisse id massa elementum, posuere turpis sed, rutrum diam. Nullam eu mollis nunc. Nunc eu est nec orci dictum condimentum.</p>
+<h1>Unordered List</h1>
+<ul>
+	<li>Item 1</li>
+	<li>Item 2</li>
+</ul>
+<h1>Ordered List</h1>
+<ol>
+	<li>Item 1</li>
+	<li>Item 2</li>
+</ol>
+<h4>H4 <strong>bold</strong></h4>
+<h6><em>em</em> H6</h6>
+
+</body>
+</html>
