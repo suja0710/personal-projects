42064c175eb0ab3d404fac5fa638b6fa604cc72f
suja chackochan <sujachakochan@gmail.com>
2019-10-14 17:34:38 -0400

    Create pokemon.php
    add table with img and  a tags

---
+++ pokemon.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pokemon Chart</title>
+</head>
+<body>
+
+	<h1>First Gen Pokemon Chart</h1>
+
+	<table>
+		<thead>
+			<tr>
+				<th>Image</th>
+				<th>Name</th>
+				<th>Type</th>
+				<th>Ivolves Into</th>
+			</tr>
+		</thead>
+			<tr>
+				<td>Bulbasaur</td>
+				<td><img src="http://img4.wikia.nocookie.net/__cb20140328190757/pokemon/images/thumb/2/21/001Bulbasaur.png/200px-001Bulbasaur.png
+Type
+"></td>
+				<td>Grass/Poison</td>
+				<td><a href="http://pokemon.wikia.com/wiki/Ivysaur">Ivysaur</a></td>
+			</tr>
+
+			<tr>
+				<td>Charmander
+</td>
+				<td><img src="http://img4.wikia.nocookie.net/__cb20140724195345/pokemon/images/thumb/7/73/004Charmander.png/200px-004Charmander.png
+"></td>
+				<td>Fire
+</td>
+				<td><a href="http://pokemon.wikia.com/wiki/Charmeleon">Charmeleon</a></td>
+			</tr>
+		<tbody>
+			
+		</tbody>
+	</table>
+
+</body>
+</html>
